bea6ae09d382fd0416767ae243bb33e25969e9e6
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2022-06-11 11:40:00 +0530

    Add files via upload

---
+++ Moonscript.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+window.addEventListener('scroll', function () {
+    var value = window.scrollY;
+    document.getElementById('stars').style.left = value * 0.25 + "px";
+    document.getElementById('moon').style.top = value * 1.05 + "px";
+    document.getElementById('mountain_behind').style.top = value * 0.5 + "px";
+    document.getElementById('mountains_front').style.top = value*0 + "px";
+    document.getElementById('text').style.marginRight = value*4 +"px";
+    document.getElementById('text').style.marginTop = value*1.5 +"px";
+    document.getElementById('btn').style.marginTop = value*1.5 + "px";
+    document.getElementBy('header1').style.top = value*0.5 +"px";
+
+
+
+
+})
+
+
+
